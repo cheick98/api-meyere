a8ca58608253f78158cee37241a5d98f5fd4cc9c
Logineo <c.diabate@logineo.com>
2023-05-08 18:49:44 +0000

    teste de vitepay

--- models/enchere.model.js
+++ models/enchere.model.js
@@ -18,7 +18,7 @@ const enchere_sch = new mongoose.Schema({
     history: [{
         buyerID: { type: mongoose.Types.ObjectId, ref: "User", required: true },
         montant: { type: Number, required: true },
-        reserve_price: { type: Boolean, default: false },
+        reserve_price: { type: Boolean },
         date: { type: Number, default: new Date().getTime() }
     }],
     likes: [{ type: mongoose.Types.ObjectId, ref: "User", required: true }],
